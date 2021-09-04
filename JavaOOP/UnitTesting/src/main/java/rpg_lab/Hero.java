017d6e95c62a5ec4e0f423fed517d094627882bf
vkarakolev <vkarakolev09@gmail.com>
2021-09-04 16:33:16 +0300

    Added unit testing exercises

---
+++ JavaOOP/UnitTesting/src/main/java/rpg_lab/Hero.java
@@ -0,0 +1,48 @@
+package rpg_lab;
+
+import rpg_lab.interfaces.Target;
+import rpg_lab.interfaces.Weapon;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Hero {
+
+    private String name;
+    private int experience;
+    private Weapon weapon;
+    private List<Weapon> inventory;
+
+    public Hero(String name, Weapon weapon) {
+        this.name = name;
+        this.experience = 0;
+        this.weapon = weapon;
+        this.inventory = new ArrayList<>();
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getExperience() {
+        return this.experience;
+    }
+
+    public Weapon getWeapon() {
+        return this.weapon;
+    }
+
+    public void attack(Target target) {
+        this.weapon.attack(target);
+
+        if (target.isDead()) {
+            this.experience += target.giveExperience();
+            this.inventory.add(target.getLoot());
+        }
+    }
+
+    public List<Weapon> getInventory() {
+        return inventory;
+    }
+}
