28912ce67ba1d1c638a35a33bc693876758ad36d
Valentin Karakolev <vkarakolev09@gmail.com>
2022-03-31 03:20:39 +0300

    JSON processing exercises added

---
+++ JavaDB/SpringData/AutoMappingObjectsEx/src/main/java/AutoMappingObjectsEx/GsonTest.java
@@ -0,0 +1,70 @@
+package AutoMappingObjectsEx;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.annotations.Expose;
+
+public class GsonTest {
+
+    private static final String json =
+                """
+                    {
+                      "username": "name",
+                      "password": "1234"
+                    }
+                                        
+                """;
+
+    public static void main(String[] args) {
+        Gson gson = new GsonBuilder()
+                .excludeFieldsWithoutExposeAnnotation()
+                .setPrettyPrinting()
+                .create();
+
+//        LoginData loginData = new LoginData("name", "1234");
+//        String result = gson.toJson(loginData);
+//        System.out.println(result);
+
+        LoginData loginData = gson.fromJson(json, LoginData.class);
+        System.out.println(loginData);
+    }
+
+    static class LoginData {
+
+        @Expose
+        private String username;
+
+        @Expose
+        private String password;
+
+        public LoginData(String username, String password) {
+            this.username = username;
+            this.password = password;
+        }
+
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+
+        @Override
+        public String toString() {
+            return "LoginData{" +
+                    "username='" + username + '\'' +
+                    ", password='" + password + '\'' +
+                    '}';
+        }
+    }
+
+}
--- JavaDB/SpringData/AutoMappingObjectsEx/src/main/java/AutoMappingObjectsEx/services/ExecutorService.java
+++ JavaDB/SpringData/AutoMappingObjectsEx/src/main/java/AutoMappingObjectsEx/services/ExecutorService.java
@@ -4,11 +4,21 @@ public interface ExecutorService {
     String REGISTER_USER_COMMAND = "RegisterUser";
     String LOGIN_USER_COMMAND = "LoginUser";
     String LOGOUT_USER_COMMAND = "LogoutUser";
+
     String ADD_GAME_COMMAND = "AddGame";
     String EDIT_GAME_COMMAND = "EditGame";
     String DELETE_GAME_COMMAND = "DeleteGame";
 
-
-
     String execute(String command);
+
+    String REGISTER_USER_JSON = """
+            RegisterUser|<email>|<password>|<confirmPassword>|<fullName> 
+            {               
+                "commandName": "RegisterUser",
+                "email": "assd@asd.com",
+                "password": "111123",
+                "confirmPassword": 111123",
+                "fullName": "Ivan Ivanov"
+            }
+            """;
 }
