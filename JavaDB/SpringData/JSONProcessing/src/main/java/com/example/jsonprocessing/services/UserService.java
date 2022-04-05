5ec12c4b8cb941f4e1ec8f1bbb23b515f5a90334
Valentin Karakolev <vkarakolev09@gmail.com>
2022-04-05 19:47:53 +0300

    XML processing exercises added

--- JavaDB/SpringData/JSONProcessing/src/main/java/com/example/jsonprocessing/services/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.example.jsonprocessing.services;
-
-import com.example.jsonprocessing.entities.users.UserWithSoldProductsDTO;
-
-import java.util.List;
-
-public interface UserService {
-    List<UserWithSoldProductsDTO> getAllWithSoldProducts();
-}
