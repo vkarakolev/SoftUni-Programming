94137f38429809380e5beccc6d38f1c8da7becc7
Valentin Karakolev <vkarakolev09@gmail.com>
2022-12-07 17:02:58 +0200

    DOM manipulation and events exercises added.

--- JavaScriptAdvanced/JSAdvanced/DOMManipulationsAndEventsEx/03. Locked-Profile/app.js
+++ JavaScriptAdvanced/JSAdvanced/DOMManipulationsAndEventsEx/03. Locked-Profile/app.js
@@ -1,3 +1,28 @@
 function lockedProfile() {
-    console.log('TODO...')
+    let users = Array.from(document.getElementsByClassName('profile'));
+
+    for(let user of users) {
+        let button = user.getElementsByTagName('button')[0];
+        button.addEventListener('click', showOrHide);
+    }
+
+    function showOrHide(e) {
+        let user = e.target.parentElement;
+        let unlockRadio = user.querySelectorAll('input[type=radio]')[1];
+        
+        if(!unlockRadio.checked) {
+            return;
+        }
+        
+        let button = e.target;
+        let hidden = user.getElementsByTagName('div')[0];
+        
+        if(button.textContent === 'Show more'){
+            hidden.style.display = 'block';
+            button.textContent = 'Hide it';
+        } else {
+            hidden.style.display = 'none';
+            button.textContent = 'Show more'; 
+        }
+    }
 }
--- JavaScriptAdvanced/JSAdvanced/DOMManipulationsAndEventsEx/04. Fill-Dropdown/dropdown.js
+++ JavaScriptAdvanced/JSAdvanced/DOMManipulationsAndEventsEx/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,14 @@
 function addItem() {
-    console.log('TODO:...');
+    let inputText = document.querySelector('#newItemText');
+    let inputValue = document.querySelector('#newItemValue');
+
+    let option = document.createElement('option');
+    option.textContent = inputText.value;
+    option.value = inputValue.value;
+
+    let menu = document.querySelector('#menu');
+    menu.appendChild(option);
+
+    inputText.value = '';
+    inputValue.value = '';
 }
