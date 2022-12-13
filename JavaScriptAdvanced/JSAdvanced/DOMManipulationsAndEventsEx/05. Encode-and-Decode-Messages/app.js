a79f7e77d48dc4f952b2b730cc41772eb802e829
Valentin Karakolev <vkarakolev09@gmail.com>
2022-12-13 20:50:45 +0200

    DOM manipulations and events exercises added.

--- JavaScriptAdvanced/JSAdvanced/DOMManipulationsAndEventsEx/05. Encode-and-Decode-Messages/app.js
+++ JavaScriptAdvanced/JSAdvanced/DOMManipulationsAndEventsEx/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,33 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    let buttons = document.querySelectorAll('button');
+    buttons[0].addEventListener('click', encode);
+    buttons[1].addEventListener('click', decode);
+    let textAreas = document.querySelectorAll('textarea');
+    let sender = textAreas[0];
+    let reciever = textAreas[1];
+
+    function encode(e) {
+        let encoded = '';
+        let currentText = sender.value;
+
+        for(let i = 0; i < currentText.length; i++) {
+            let currentCh = currentText[i].charCodeAt();
+            encoded += String.fromCharCode(currentCh + 1);
+        }
+
+        reciever.value = encoded;
+        sender.value = '';
+    }
+    
+    function decode() {
+        let decoded = '';
+        let currentText = reciever.value;
+
+        for(let i = 0; i < currentText.length; i++) {
+            let currentCh = currentText[i].charCodeAt();
+            decoded += String.fromCharCode(currentCh - 1);
+        }
+
+        reciever.value = decoded;       
+    }
 }
