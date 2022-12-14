48b854e68df2a79f2371e09a3127d79f0835c0bb
Valentin Karakolev <vkarakolev09@gmail.com>
2022-12-14 12:54:04 +0200

    DOM manipulations and event exercises added.

--- JavaScriptAdvanced/JSAdvanced/DOMManipulationsAndEventsEx/07. Distance-Converter/distanceConverter.js
+++ JavaScriptAdvanced/JSAdvanced/DOMManipulationsAndEventsEx/07. Distance-Converter/distanceConverter.js
@@ -1,3 +1,67 @@
 function attachEventsListeners() {
-    console.log('TODO:...');
+    let button = document.querySelector('#convert').addEventListener('click', convert);
+
+    function convert(e) {
+        let input = e.target.parentElement.children[1].value;
+        let from = e.target.parentElement.children[2].value;
+
+        let distanceInM = 0; 
+        switch(from) {
+            case 'km':
+                distanceInM = input * 1000
+                break;
+            case 'm': 
+                distanceInM = input;
+                break;
+            case 'cm':
+                distanceInM = input * 0.01;
+                break;
+            case 'mm':
+                distanceInM = input * 0.001;
+                break;
+            case 'mi':
+                distanceInM = input * 1609.34;
+                break;
+            case 'yrd':
+                distanceInM = input * 0.9144;
+                break;
+            case 'ft':
+                distanceInM = input * 0.3048;
+                break;
+            case 'in':
+                distanceInM = input * 0.0254;
+                break;
+        }
+
+        let to = document.querySelector('#outputUnits').value;       
+        let result = 0;
+        switch(to) {
+            case 'km':
+                result = distanceInM / 1000;
+                break;
+            case 'm': 
+                result = distanceInM;
+                break;
+            case 'cm':
+                result = distanceInM / 0.01;
+                break;
+            case 'mm':
+                result = distanceInM / 0.001;
+                break;
+            case 'mi':
+                result = distanceInM / 1609.34;
+                break;
+            case 'yrd':
+                result = distanceInM / 0.9144;
+                break;
+            case 'ft':
+                result = distanceInM / 0.3048;
+                break;
+            case 'in':
+                result = distanceInM / 0.0254;
+                break;
+        }
+
+        document.querySelector('#outputDistance').value = result;
+    }
 }
