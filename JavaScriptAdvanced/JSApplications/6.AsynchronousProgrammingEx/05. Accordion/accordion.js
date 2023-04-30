68161939cf75bf30268f1e6ac53a00ae6280381f
Valentin Karakolev <vkarakolev09@gmail.com>
2023-04-30 17:37:11 +0300

    Asynchronous programming exercises added.

--- JavaScriptAdvanced/JSApplications/6.AsynchronousProgrammingEx/05. Accordion/accordion.js
+++ JavaScriptAdvanced/JSApplications/6.AsynchronousProgrammingEx/05. Accordion/accordion.js
@@ -1,3 +1,48 @@
 function solution() {
-    //TODO .....
+    window.addEventListener('load', attachArticles);
 }
+
+async function attachArticles() {
+    const response = await fetch('http://localhost:3030/jsonstore/advanced/articles/list');
+    const articles = await response.json();
+
+    const main = document.getElementById('main')
+    articles.forEach(a => {
+        main.innerHTML += `
+        <div class="accordion">
+            <div class="head">
+                <span>${a.title}</span>
+                <button class="button" id="${a._id}">More</button>
+            </div>
+            <div class="extra">
+                <p></p>
+            </div>
+        </div>`;
+        
+        let button = document.getElementById(a._id);
+        button.addEventListener('click', toggleContent);
+    });
+}
+
+async function toggleContent(e) {
+    const button = e.target;
+    const article = button.parentElement.parentElement;
+    const extra = article.children[1];
+
+    const response = await fetch('http://localhost:3030/jsonstore/advanced/articles/details/' + button.id);
+    const data = await response.json();
+
+    const p = extra.children[0];
+    p.textContent = data.content;
+
+    
+    if(extra.classList.contains('extra')) {
+        button.textContent = 'Less';
+        extra.classList.remove('extra');
+    } else {
+        button.textContent = 'More';
+        extra.classList.add('extra');
+    };
+}
+
+solution();
