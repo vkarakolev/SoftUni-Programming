2863574ef898aa44e13e00a9a7eae6f8f61cf2a4
Valentin Karakolev <vkarakolev09@gmail.com>
2023-05-14 17:08:56 +0300

    Remote data and authentication exercises added.

--- JavaScriptAdvanced/JSApplications/8.RemoteDataAndAuthenticationEx/03.Students/app.js
+++ JavaScriptAdvanced/JSApplications/8.RemoteDataAndAuthenticationEx/03.Students/app.js
@@ -1,1 +1,64 @@
-console.log('TODO...');
+window.onload = attachEvents;
+
+function attachEvents() {
+    document.getElementById('form').addEventListener('submit', onSubmit);
+}
+
+function onSubmit(e) {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const {firstName, lastName, facultyNumber, grade} = Object.fromEntries(formData.entries());
+    e.target.reset();
+
+    const url = 'http://localhost:3030/jsonstore/collections/students';
+    const body = {
+        firstName: firstName,
+        lastName: lastName,
+        facultyNumber: facultyNumber,
+        grade: grade
+    }
+
+    sendStudentData(url, body);
+}
+
+async function sendStudentData(url, body) {
+    const response = await fetch(url,{
+        method: 'POST',
+        headers: {
+            'Constent-Type': 'application/json'
+        },
+        body: JSON.stringify(body)
+    });
+
+    const data = await response.json();
+    getAllStudents();
+}
+
+async function getAllStudents() {
+    const url = 'http://localhost:3030/jsonstore/collections/students';
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    renderStudents(Object.values(data));
+    return data;
+}
+
+function renderStudents(data) {
+    const tbody = document.querySelector('tbody');
+
+    data.forEach(e => {
+        const tr = document.createElement('tr');
+        Object.entries(e).forEach(([k, v]) => {
+            if(k === '_id') {
+                return;
+            }
+
+            const td = document.createElement('td');
+            td.textContent = v;
+            tr.appendChild(td);
+        });
+
+        tbody.appendChild(tr);
+    });
+}
--- JavaScriptAdvanced/JSApplications/8.RemoteDataAndAuthenticationEx/04.Book-library/app.js
+++ JavaScriptAdvanced/JSApplications/8.RemoteDataAndAuthenticationEx/04.Book-library/app.js
@@ -1,1 +1,127 @@
-console.log('My requests...')
+window.onload = attachEvents;
+const url = `http://localhost:3030/jsonstore/collections/books`;
+
+function attachEvents() {
+    document.getElementById('loadBooks').addEventListener('click', loadAllBooks);
+    document.querySelector('form').addEventListener('submit', onSubmit);
+}
+
+async function loadAllBooks() {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    renderBooks(Object.entries(data));
+    return data;
+}
+
+function renderBooks(data) {
+    const tbody = document.querySelector('tbody');
+    tbody.innerHTML = '';
+
+    data.forEach(([k, v]) => {
+        const tr = document.createElement('tr');
+        tr.setAttribute('bookId', k);
+        tr.innerHTML = `
+            <td>${v.title}</td>
+            <td>${v.author}</td>`;
+
+        const td = document.createElement('td');
+        const editBtn = document.createElement('button');
+        editBtn.textContent = 'Edit';
+        editBtn.addEventListener('click', onEdit);
+
+        const deleteBtn = document.createElement('button');
+        deleteBtn.textContent = 'Delete';
+        deleteBtn.addEventListener('click', onDelete);
+
+        td.appendChild(editBtn);
+        td.appendChild(deleteBtn);
+        tr.appendChild(td);
+        tbody.appendChild(tr);
+    });
+}
+
+function onDelete(e) {
+    const tr = e.target.parentElement.parentElement;
+    const id = tr.getAttribute('bookId');
+
+    deleteBook(id);
+    tr.remove();
+}
+
+async function deleteBook(id) {
+    const response = await fetch(url + `/${id}`, {
+        method: 'DELETE',
+    });
+
+    const data = await response.json();
+    return data;
+}
+
+function onEdit(e) {
+    const form = document.querySelector('form');
+    const tr = e.target.parentElement.parentElement;
+    form.setAttribute('editId', tr.getAttribute('bookId'));
+
+    form.children[0].textContent = 'Edit FORM';
+    form.title.value = tr.children[0].textContent;
+    form.author.value = tr.children[1].textContent;
+    form.children[5].textContent = 'Save';
+}
+
+async function editBook(body) {
+    const form = document.querySelector('form');
+    const id = form.getAttribute('editId');
+
+    const response = await fetch(url + `/${id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(body)
+    });
+
+    const data = await response.json();
+
+    form.children[0].textContent = 'FORM';
+    form.children[5].textContent = 'Submit';
+    loadAllBooks();
+}
+
+function onSubmit(e) {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const {title, author} = Object.fromEntries(formData.entries());
+    const formLabel = e.target.children[0];
+
+    if(!title || !author) {
+        return;
+    }
+    
+    const body = {
+        author: author,
+        title: title
+    };
+
+    
+    if(formLabel.textContent !== 'Edit FORM') {
+        createBook(body);
+    } else {
+        editBook(body);
+    }
+    
+    e.target.reset();
+}
+
+async function createBook(body) {
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(body)
+    });
+
+    const data = await response.json();
+    loadAllBooks();
+}
