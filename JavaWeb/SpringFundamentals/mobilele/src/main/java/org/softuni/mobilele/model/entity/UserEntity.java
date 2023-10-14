37ff453b38617564d6d67e07707bd5bdc2164768
Valentin Karakolev <vkarakolev09@gmail.com>
2023-10-14 18:11:09 +0300

    User login and logout features added.

--- JavaWeb/SpringFundamentals/mobilele/src/main/java/org/softuni/mobilele/model/entity/UserEntity.java
+++ JavaWeb/SpringFundamentals/mobilele/src/main/java/org/softuni/mobilele/model/entity/UserEntity.java
@@ -1,11 +1,13 @@
 package org.softuni.mobilele.model.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "users")
 public class UserEntity extends BaseEntity {
+    @Column(unique = true)
     private String email;
 
     private String password;
