55b27c26bacb024149e8883477c98c2e733ef676
Valentin Karakolev <vkarakolev09@gmail.com>
2023-10-19 17:03:52 +0300

    Reseller app: database and conditions initialization

--- JavaWeb/SpringFundamentals/mobilele/src/main/java/org/softuni/mobilele/model/dto/UserRegistrationDTO.java
+++ JavaWeb/SpringFundamentals/mobilele/src/main/java/org/softuni/mobilele/model/dto/UserRegistrationDTO.java
@@ -2,8 +2,6 @@ package org.softuni.mobilele.model.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
-import org.apache.catalina.User;
 import org.softuni.mobilele.model.validation.FieldMatch;
 import org.softuni.mobilele.model.validation.UniqueUserEmail;
 
